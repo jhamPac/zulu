12158809d949ca7cabce6d5d00c0afcb4fb256cf
jhamPac <cloud@kevia.me>
2020-03-08 16:04:24 -0700

    save point

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    printWidth: 90,
+    semi: true,
+    singleQuote: true,
+    trailingComma: "es5",
+    tabWidth: 4
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: './index.js',
+        library: '',
+        libraryTarget: 'commonjs',
+    },
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.(jsx?)$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader', options: { rootMode: 'upward' } },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss'],
+        modules: [path.join(__dirname, 'node_modules')],
+        plugins: [new DirectoryNamedWebpackPlugin()],
+    },
+};
