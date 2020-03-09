72d65a516b71759e38fce2f5fe0017212e8230b7
jhamPac <cloud@kevia.me>
2020-03-09 13:58:57 -0700

    save point

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,23 @@ module.exports = {
     output: {
         filename: './index.js',
         library: '',
-        libraryTarget: 'commonjs',
+        libraryTarget: 'umd',
+    },
+    externals: {
+        react: {
+            commonjs: 'React',
+            commonjs2: 'React',
+            amd: 'React',
+            umd: 'React',
+            root: 'React',
+        },
+        'material-ui': {
+            commonjs: 'material-ui',
+            commonjs2: 'material-ui',
+            amd: 'material-ui',
+            umd: 'material-ui',
+            root: 'material-ui',
+        },
     },
     target: 'web',
     module: {
@@ -27,6 +43,9 @@ module.exports = {
         ],
     },
     resolve: {
+        alias: {
+            react: path.resolve(__dirname, '../node_modules/react'),
+        },
         extensions: ['.js', '.jsx', '.css', '.scss'],
         modules: [path.join(__dirname, 'node_modules')],
         plugins: [new DirectoryNamedWebpackPlugin()],
